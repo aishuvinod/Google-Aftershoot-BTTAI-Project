508f855e1cff18ca82988e7b61b706abf1085b30
isabellelwang <144399468+isabellelwang@users.noreply.github.com>
2024-10-03 19:18:29 -0400

    Add files via upload
    Updated code

--- imagereader.py
+++ imagereader.py
@@ -7,6 +7,7 @@ import cv2
 labels = []
 images = []
 
+
 def read_image(data_dir, folder) :
   # retrieving folder path by joining the data directory and folder
   folder_path = os.path.join(data_dir, folder)
@@ -18,11 +19,17 @@ def read_image(data_dir, folder) :
   for img_jpg in image_list:
     image_path = os.path.join(folder_path, img_jpg)
     read_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    resized_image = cv2.resize(read_img, (64,64))
+    #resized_img_flatten = resized_image.reshape(-1)
+    #print(resized_img_flatten.shape)
+    
     labels.append(folder)
 
     if read_img is not None:
-      img_data = read_img
+      img_data = resized_image
       flatten_img = img_data.flatten()
+      print(flatten_img.shape); 
+      #print(flatten_img.shape)
       images.append(flatten_img)
     else:
       print('None')
@@ -36,13 +43,19 @@ def main():
        if folder != ".DS_Store": 
           read_image(data, folder)
 
+    
+
 
 if __name__ == '__main__': 
     print("Reading data")
     main()
-    df = pd.DataFrame(data = images)
+    
+    df = pd.DataFrame(data = images, columns = [i for i in range(0, images[0].shape[0])])
     df['label'] = labels
-    df.to_csv('Eurodataset_preprocessing.csv', index = False)
+
+    #data = df.iloc[:50]
+    df.to_csv("Eurodataset_preprocessing.csv", index = "image_name"); 
+
     print("Data stored in a csv complete!")
 
     
